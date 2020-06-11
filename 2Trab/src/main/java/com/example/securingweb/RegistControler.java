3fbf53259b625a0d7408117e189c869f7d56d0db
Pedro Amaro <l42348@alunos.uevora.pt>
2020-06-11 15:50:10 +0100

    Com mapa ainda por trabalhar

--- 2Trab/src/main/java/com/example/securingweb/RegistControler.java
+++ 2Trab/src/main/java/com/example/securingweb/RegistControler.java
@@ -52,7 +52,7 @@ public class RegistControler extends HttpServlet {
        return makeTable();
     }
 
-    public String makeTable() throws IOException {
+    public String makeTable(){
         //response.setContentType("text/html");
         Iterable<Registo> all = registoRepository.getTest();
         String lines="";
@@ -71,7 +71,10 @@ public class RegistControler extends HttpServlet {
         reg2=false;
 
         Iterator<Registo> iterador = all.iterator();
-        iterador.next();
+        try{
+            iterador.next();
+        }catch (RuntimeException targetException) {}
+
         for (Registo registo : all) {
 
             if (iterador.hasNext())
@@ -181,8 +184,41 @@ public class RegistControler extends HttpServlet {
         return "<html>\n" +
                 "<head>\n" +
                 "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/table_css.css\">\n"+
+                "<link rel=\"stylesheet\" href=\"https://unpkg.com/leaflet@1.6.0/dist/leaflet.css\"\n" +
+                "   integrity=\"sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==\"\n" +
+                "   crossorigin=\"\"/>\n" +
+                "<script src=\"https://unpkg.com/leaflet@1.6.0/dist/leaflet.js\"\n" +
+                "   integrity=\"sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==\"\n" +
+                "   crossorigin=\"\"></script>"+
                 "</head>\n"  +
-                ("<body>\n")+("<div class=\"form\">\n")+
+                ("<body>\n")+"<div class=\"header\">\n" +
+                "        <a href=\"/home\" class=\"logo\">CompanyLogo</a>\n" +
+                "        <div class=\"header-right\">\n" +
+                "            <a class=\"active\" href=\"/userPanel\">Control Panel</a>\n" +
+                "            <a class=\"active\">\n" +
+                "            <form th:action=\"@{/logout}\" method=\"post\">\n" +
+                "                    <button>Sign Out</button>\n" +
+                "                </form>\n" +
+                "            </a>\n" +
+                "        </div>\n" +
+                "    </div>\n"+
+                " <div id=\"mapid\" style=\"width: 600px; height: 400px; position: relative; outline: none;\" class=\"leaflet-container leaflet-retina leaflet-fade-anim leaflet-grab leaflet-touch-drag\" tabindex=\"0\"><div class=\"leaflet-pane leaflet-map-pane\" style=\"transform: translate3d(0px, 0px, 0px);\"><div class=\"leaflet-pane leaflet-tile-pane\"><div class=\"leaflet-layer \" style=\"z-index: 1; opacity: 1;\"><div class=\"leaflet-tile-container leaflet-zoom-animated\" style=\"z-index: 18; transform: translate3d(0px, 0px, 0px) scale(1);\"><img alt=\"\" role=\"presentation\" src=\"https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/12/2046/1361?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw\" class=\"leaflet-tile leaflet-tile-loaded\" style=\"width: 512px; height: 512px; transform: translate3d(-200px, -347px, 0px); opacity: 1;\"><img alt=\"\" role=\"presentation\" src=\"https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/12/2047/1361?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw\" class=\"leaflet-tile leaflet-tile-loaded\" style=\"width: 512px; height: 512px; transform: translate3d(312px, -347px, 0px); opacity: 1;\"><img alt=\"\" role=\"presentation\" src=\"https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/12/2046/1362?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw\" class=\"leaflet-tile leaflet-tile-loaded\" style=\"width: 512px; height: 512px; transform: translate3d(-200px, 165px, 0px); opacity: 1;\"><img alt=\"\" role=\"presentation\" src=\"https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/12/2047/1362?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw\" class=\"leaflet-tile leaflet-tile-loaded\" style=\"width: 512px; height: 512px; transform: translate3d(312px, 165px, 0px); opacity: 1;\"></div></div></div><div class=\"leaflet-pane leaflet-shadow-pane\"></div><div class=\"leaflet-pane leaflet-overlay-pane\"></div><div class=\"leaflet-pane leaflet-marker-pane\"></div><div class=\"leaflet-pane leaflet-tooltip-pane\"></div><div class=\"leaflet-pane leaflet-popup-pane\"></div><div class=\"leaflet-proxy leaflet-zoom-animated\" style=\"transform: translate3d(1.04805e+06px, 697379px, 0px) scale(4096);\"></div></div><div class=\"leaflet-control-container\"><div class=\"leaflet-top leaflet-left\"><div class=\"leaflet-control-zoom leaflet-bar leaflet-control\"><a class=\"leaflet-control-zoom-in\" href=\"#\" title=\"Zoom in\" role=\"button\" aria-label=\"Zoom in\">+</a><a class=\"leaflet-control-zoom-out\" href=\"#\" title=\"Zoom out\" role=\"button\" aria-label=\"Zoom out\">−</a></div></div><div class=\"leaflet-top leaflet-right\"></div><div class=\"leaflet-bottom leaflet-left\"></div><div class=\"leaflet-bottom leaflet-right\"><div class=\"leaflet-control-attribution leaflet-control\"><a href=\"https://leafletjs.com\" title=\"A JS library for interactive maps\">Leaflet</a> | Map data © <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a></div></div></div></div>"+
+                "<script>\n" +
+                "\n" +
+                "\tvar mymap = L.map('mapid').setView([51.505, -0.09], 13);\n" +
+                "\n" +
+                "\tL.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {\n" +
+                "\t\tmaxZoom: 18,\n" +
+                "\t\tattribution: 'Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, ' +\n" +
+                "\t\t\t'<a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, ' +\n" +
+                "\t\t\t'Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>',\n" +
+                "\t\tid: 'mapbox/streets-v11',\n" +
+                "\t\ttileSize: 512,\n" +
+                "\t\tzoomOffset: -1\n" +
+                "\t}).addTo(mymap);\n" +
+                "\n" +
+                "</script>"+
+                ("<div class=\"form\">\n")+
                 ("<table>\n" +
                 "  <tr>\n" +
                 "    <th>Place</th>\n" +
